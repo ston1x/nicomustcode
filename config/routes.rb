8800cbf85a3ab29a6c70b57a4586ade29334b9ad
Nicolai Stoianov <stoianovnk@gmail.com>
2018-02-03 00:47:41 +0200

    Experimenting with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  root 'people#index'
+  resources :people
   resources :articles
   devise_for :users
-  resources :people
   resources :users do
   	resources :articles, shallow: true
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'people', to: "people#index"
-  root 'people#index'
 end

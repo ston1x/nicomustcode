36f8ae840aae022216eeceebbe68dde7ea75383e
Nicolai Stoianov <stoianovnk@gmail.com>
2018-02-03 00:09:37 +0200

    fixing No Such App error in Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   	resources :articles, shallow: true
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'people', to: "people#index"
   root 'people#index'
 end
